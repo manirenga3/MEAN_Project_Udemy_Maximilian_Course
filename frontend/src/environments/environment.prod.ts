a58c1dc730f53fe5c335145ec826aec7e3cd42f5
Manikandan Renga <manikandanrenga3@gmail.com>
2022-03-15 10:51:15 +0530

    Initial commit

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  rootUrl:
-    'http://meanapp-env-1.eba-bwsb3byn.ap-south-1.elasticbeanstalk.com/api',
+  rootUrl: 'http://mean-app-manirenga3.ap-south-1.elasticbeanstalk.com/api',
 };
